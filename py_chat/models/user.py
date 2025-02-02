5be01efe7d5a4677bf53334dd4972ad63fc6737d
Humberto <matheushumberto520@gmail.com>
2025-02-01 20:39:50 -0400

    feat(models): Will create a model for friends

--- py_chat/models/user.py
+++ py_chat/models/user.py
@@ -1,8 +1,8 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column, registry
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, registry, relationship
 
 table_registry = registry()
 
@@ -12,7 +12,7 @@ class User:
     __tablename__ = 'users'
 
     id: Mapped[str] = mapped_column(
-        init=False, primary_key=True, default=str(uuid.uuid4())
+        init=False, primary_key=True, default_factory=lambda: str(uuid.uuid4())
     )
     username: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str] = mapped_column(unique=True)
@@ -23,4 +23,25 @@ class User:
     )
     updated_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now(), onupdate=func.now()
+    )
+
+
+@table_registry.mapped_as_dataclass
+class Friend:
+    __tablename__ = 'friends'
+
+    user_id: Mapped[str] = mapped_column(
+        ForeignKey('users.id', ondelete='CASCADE'), primary_key=True
+    )
+    friend_id: Mapped[str] = mapped_column(
+        ForeignKey('users.id', ondelete='CASCADE'), primary_key=True
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    user: Mapped['User'] = relationship(
+        'User', init=False, foreign_keys=[user_id], backref='friends'
+    )
+    friend: Mapped['User'] = relationship(
+        'User', init=False, foreign_keys=[friend_id]
     )
